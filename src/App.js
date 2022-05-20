52a689f64f5a43688370f9675b4d7bb373ed2828
bturan27 <begumturann@outlook.com>
2022-05-19 22:32:00 -0700

    Pokemon

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import React, {useState} from "react";
 import './App.css';
+import PokeCard from './components/PokeCard';
 
 function App() {
+  const [pokeList, setPokeList] = useState([
+    "bidoof","klefki","magikarp","vanillish","trubbish"
+  ]);
+  const [input, setInput] = useState("");
+
+  const submitHandler =(event) =>{
+    event.preventDefault();
+    setPokeList([...pokeList,input]);
+    setInput("");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> make the strongest pokemon team</h1>
+      <form className="w-25 d-block mx-auto my-3" onSubmit={submitHandler}>
+          <input type="text" placeholder="add to your team"  onChange={(event)=>{
+            setInput(event.target.value) }}></input>
+          <input type="submit" className="btn btn-info d-block mx-auto my-1" />
+      </form>
+      {/* <PokeCard name="pikachu"/> */}
+      <div className="list">
+      {
+        pokeList.map((item,i)=>{
+          return <PokeCard key={i} name={item}/>
+        })
+      }
+      </div>
+      
     </div>
   );
 }
